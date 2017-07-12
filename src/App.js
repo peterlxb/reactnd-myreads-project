64b5ee880b9ded6ad4ed85f6fe0bea5ec54e122b
peterliuxb <woairuidaren@gmail.com>
2017-07-12 09:21:49 +0800

    refector

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     // showSearchPage: true
+    books:[]
   }
 
 
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class SearchBooks extends React.Component {
 
   state = {
-    search:''
+    query:''
+  }
+
+  updateQuery = (query) => {
+    this.setState({query:query.trim()})
   }
 
   render() {
+
+
+
     return (
+
       <div className="search-books">
+
         <div className="search-books-bar">
 
           <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => {
+                this.updateQuery(event.target.value);
+                
+              }}
+            />
+            {JSON.stringify(this.state)}
           </div>
+
         </div>
+
         <div className="search-books-results">
           <ol className="books-grid"></ol>
         </div>
