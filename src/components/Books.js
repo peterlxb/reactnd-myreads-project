81ed7451519c891b91076480e2b395a9917b7f4e
peterliuxb <woairuidaren@gmail.com>
2017-07-12 16:32:49 +0800

    use the api to get data

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,11 +4,26 @@ import React from 'react'
 
 class Books extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {value:'none'}
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+    handleChange(event) {
+      this.setState({value: event.target.value})
+    };
+
+
+
   render(){
+
+    const {books} =  this.props
+
     return(
       <ol className="books-grid">
-        {this.props.books.map((book) => (
-          
+        {books.map((book) => (
+
           <li key={book.id}>
           <div className="book">
             <div className="book-top">
@@ -16,7 +31,7 @@ class Books extends React.Component {
                 <img src={book.imageURL} alt={book.title}/>
               </div>
               <div className="book-shelf-changer">
-                <select>
+                <select value={this.state.value} onChange={this.handleChange}>
                   <option value="none" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
